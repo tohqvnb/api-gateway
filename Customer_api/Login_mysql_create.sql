b4a4d500338cd88a0599b6a78ff7aac6fe8a900e
JustAbusiness <huysanti123456@gmail.com>
2023-03-06 14:59:10 +0700

    Edit database schema

--- Customer_api/Login_mysql_create.sql
+++ Customer_api/Login_mysql_create.sql
@@ -1,44 +1,46 @@
-CREATE TABLE `login` (
-	`email` varchar(50) NOT NULL UNIQUE,
-	`password` varchar(50) NOT NULL
-);
-
-CREATE TABLE `register` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`username` varchar(255) NOT NULL,
-	`email` varchar(50) NOT NULL UNIQUE,
-	`password` varchar(50) NOT NULL,
-	`created_at` TIMESTAMP NOT NULL,
-	`updated_at` TIMESTAMP NOT NULL,
-	PRIMARY KEY (`id`)
-);
-
 CREATE TABLE `customer` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
-	`name` varchar(255) NOT NULL,
+	`first_name` varchar(255) NOT NULL,
+	`last_name` varchar(255) NOT NULL,
 	`email` varchar(255) NOT NULL,
 	`password` varchar(255) NOT NULL,
+	`phone_number` INT(50) NOT NULL,
+	`gender` tinyint(50) NOT NULL DEFAULT '0: male , 1: female',
+	`date_of_birth` TIME NOT NULL,
 	`created_at` TIMESTAMP NOT NULL,
 	`updated_at` TIMESTAMP NOT NULL,
+	`deleted_at` tinyint(50) NOT NULL DEFAULT '0: male , 1: female',
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `address` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`customer_id` INT NOT NULL,
 	`address_line` varchar(255) NOT NULL,
-	`address_line2` varchar(255) NOT NULL,
+	`address_line2` varchar(255),
+	`address_line3` varchar(255),
 	`city` varchar(255) NOT NULL,
+	`street` varchar(255),
 	`state` varchar(255) NOT NULL,
 	`country` varchar(255) NOT NULL,
-	`zip_code` varchar(255) NOT NULL,
+	`potal_code` varchar(255) NOT NULL,
+	`created_at` TIMESTAMP NOT NULL,
+	`updated_at` TIMESTAMP NOT NULL,
+	`deleted_at` TIMESTAMP NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `customer_address` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`customer_id` INT(11) NOT NULL,
+	`address_id` INT(11) NOT NULL,
 	`created_at` TIMESTAMP NOT NULL,
 	`updated_at` TIMESTAMP NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
-ALTER TABLE `address` ADD CONSTRAINT `address_fk0` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`);
+ALTER TABLE `customer_address` ADD CONSTRAINT `customer_address_fk0` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`);
 
+ALTER TABLE `customer_address` ADD CONSTRAINT `customer_address_fk1` FOREIGN KEY (`address_id`) REFERENCES `address`(`id`);
 
 
 
